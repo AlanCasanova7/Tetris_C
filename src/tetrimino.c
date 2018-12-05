a370b02809f960e1efc895eec900b9c5381eb690
DESKTOP-1PIH112\Alan <alancasanova@hotmail.it>
2018-12-05 19:52:16 +0100

    Added support for index based coloring on both maps and tetriminos.

--- src/tetrimino.c
+++ src/tetrimino.c
@@ -24,7 +24,7 @@ tetrimino_t *tetrimino_new(char *map, uint8_t offset, int pos_x, int pos_y)
     return tetrimino;
 }
 
-void draw_tetrimino(tetrimino_t *tetrimino, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+void draw_tetrimino(tetrimino_t* tetrimino, color_t* color)
 {
     SDL_Rect rect;
     rect.w = TETRIMINO_SIZE;
@@ -33,19 +33,23 @@ void draw_tetrimino(tetrimino_t *tetrimino, uint8_t r, uint8_t g, uint8_t b, uin
     
     for (unsigned i = tetrimino->offset * TETRIMINO_SEGMENT; i < (tetrimino->offset * TETRIMINO_SEGMENT) + TETRIMINO_SEGMENT; i++)
     {
-        if (tetrimino->tetrimino_map[i] == 1)
+        if (tetrimino->tetrimino_map[i] != 0)
         {
             rect.x = (((i - (tetrimino->offset * TETRIMINO_SEGMENT)) % TETRIMINO_COLUMNS) * rect.w) + tetrimino->pos_x;
             rect.y = (((i - (tetrimino->offset * TETRIMINO_SEGMENT)) / TETRIMINO_COLUMNS) * rect.h) + tetrimino->pos_y;
 
-            SDL_SetRenderDrawColor(context->renderer, r, g, b, a);
+            SDL_SetRenderDrawColor(context->renderer, 
+            color[(int)tetrimino->tetrimino_map[i] - 1].r,
+            color[(int)tetrimino->tetrimino_map[i] - 1].g, 
+            color[(int)tetrimino->tetrimino_map[i] - 1].b, 
+            color[(int)tetrimino->tetrimino_map[i] - 1].a);
             SDL_RenderFillRect(context->renderer, &rect);
             SDL_RenderDrawRect(context->renderer, &rect);
         }
     }
 }
 
-void draw_map(char *map)
+void draw_map(char *map, color_t* color)
 {
     SDL_Rect rect;
     rect.w = TETRIMINO_SIZE;
@@ -53,12 +57,12 @@ void draw_map(char *map)
 
     for (unsigned i = 0; i < TETRIMINO_TILEMAP_LENGTH; i++)
     {
-        if (map[i] == 7)
+        if (map[i] != 0)
         {
             rect.x = ((i % MAP_COLUMNS) * TETRIMINO_SIZE);
             rect.y = ((i / MAP_COLUMNS) * TETRIMINO_SIZE);
 
-            SDL_SetRenderDrawColor(context->renderer, 255, 255, 255, 255);
+            SDL_SetRenderDrawColor(context->renderer, color[(int)map[i] -1].r, color[(int)map[i] -1].g, color[(int)map[i] -1].b, color[(int)map[i] -1].a);
             SDL_RenderFillRect(context->renderer, &rect);
             SDL_RenderDrawRect(context->renderer, &rect);
         }
--- tests/main.c
+++ tests/main.c
@@ -6,6 +6,7 @@
 
 tetrimino_t* current_tetrimino = NULL;
 sdl_context_t* context = NULL;
+color_t* colors = NULL;
 
 char pressed_spacebar = 0;
 char pressed_right = 0;
@@ -17,8 +18,6 @@ float update_counter = 0;
 void update(double delta_time){
     update_counter += delta_time;
     if(update_counter >= 1){
-        //printf("muoviti");
-        // do something every one second.
         current_tetrimino->pos_y += 20;
         update_counter = 0;
     }
@@ -35,9 +34,9 @@ static void draw(sdl_context_t* ctx, double delta_time)
 {
     update(delta_time);
 
-    draw_tetrimino(current_tetrimino, 255, 0, 0, 255);
+    draw_tetrimino(current_tetrimino, colors);
 
-    draw_map(tilemap);
+    draw_map(tilemap, colors);
 
     DO_ON_BUTTON_DOWN(ctx, SDL_SCANCODE_SPACE, pressed_spacebar, offset, 1);
     DO_ON_BUTTON_DOWN(ctx, SDL_SCANCODE_DOWN, pressed_down, pos_y, 20);
@@ -52,6 +51,16 @@ int main(int argc, char** argv)
 
     tetrimino_t* tetrimino = tetrimino_new(tetrimino_L, 2, 20, 20);
     current_tetrimino = tetrimino;
+
+    colors = malloc(sizeof(color_t)*8);
+    colors[0].r = 255;  colors[0].g = 0;    colors[0].b = 0;    colors[0].a = 255;
+    colors[1].r = 255;  colors[1].g = 255;  colors[1].b = 0;    colors[1].a = 255;
+    colors[2].r = 0;    colors[2].g = 255;  colors[2].b = 0;    colors[2].a = 255;
+    colors[3].r = 0;    colors[3].g = 255;  colors[3].b = 200;  colors[3].a = 255;
+    colors[4].r = 0;    colors[4].g = 160;  colors[4].b = 255;  colors[4].a = 255;
+    colors[5].r = 0;    colors[5].g = 0;    colors[5].b = 255;  colors[5].a = 255;
+    colors[6].r = 255;  colors[6].g = 0;    colors[6].b = 255;  colors[6].a = 255;
+    colors[7].r = 255;  colors[7].g = 255;  colors[7].b = 255;  colors[7].a = 255;
 
     sdl_context_update(context);
 
