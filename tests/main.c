8a7aa886bedcc9cc71631c02dd07172ddbb04532
luca1337 <biohazard911@hotmail.it>
2018-12-05 15:09:53 +0100

    clamped tetrimino offset in order to stay inside boundaries 0-3)

--- tests/main.c
+++ tests/main.c
@@ -12,7 +12,7 @@ static void draw(sdl_context_t* ctx)
     draw_tetrimino(current_tetrimino, 255, 0, 0, 255);
 
     if(get_key(context, SDL_SCANCODE_W))
-        current_tetrimino->pos_y -= 25;
+        current_tetrimino->pos_y -= 20;
 }
 
 int main(int argc, char** argv)
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
     context = sdl_context_new("Tetris", 640, 480, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     context->post_draw = draw;
 
-    tetrimino_t* tetrimino = tetrimino_new(tetrimino_L, 2, context->width / 2 - 50, context->height / 2 - 50);
+    tetrimino_t* tetrimino = tetrimino_new(tetrimino_L, 0, context->width / 2 - 50, context->height / 2 - 50);
     current_tetrimino = tetrimino;
 
     sdl_context_update(context);
